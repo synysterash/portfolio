23e696f2d1acc48754f00777649e6d2ad5bc624a
synysterash <97072260+synysterash@users.noreply.github.com>
2022-12-11 05:08:52 +0800

    First Commit

---
+++ resources/views/layouts/inc_footer.blade.php
@@ -0,0 +1,15 @@
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong><span>Ashman Rahman</span></strong>
+      </div>
+      <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/ -->
+        Designed by Ashman Rahman
+      </div>
+    </div>
+  </footer><!-- End  Footer -->
