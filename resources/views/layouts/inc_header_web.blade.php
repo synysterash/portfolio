d503e46310cd329205a592f3779e1b544f0a75fa
synysterash <97072260+synysterash@users.noreply.github.com>
2022-12-14 02:03:59 +0800

    Update inc_header_web.blade.php

--- resources/views/layouts/inc_header_web.blade.php
+++ resources/views/layouts/inc_header_web.blade.php
@@ -9,10 +9,10 @@
         <img src={{ asset('assets/img/dp.png')}} alt="" class="img-fluid rounded-circle">
         <h1 class="text-light"><a href="index.html">Ashman Rahman</a></h1>
         <div class="social-links mt-3 text-center">
-          <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+          <a href="https://www.twitter.com/synysterash" target=".blank" class="twitter"><i class="bx bxl-twitter"></i></a>
           <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-          <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-          <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+          <a href="https://www.instagram.com/edrisashman" target=".blank" class="instagram"><i class="bx bxl-instagram"></i></a>
+          <a href="http://wa.me/+60194944096" target=".blank" class="whatsapp"><i class="bx bxl-whatsapp"></i></a>
           <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
         </div>
       </div>
@@ -21,6 +21,7 @@
         <ul>
           <li><a href="#hero" class="nav-link scrollto active"><i class="bx bx-home"></i> <span>Home</span></a></li>
           <li><a href="#about" class="nav-link scrollto"><i class="bx bx-user"></i> <span>About</span></a></li>
+          <li><a href="#skills" class="nav-link scrollto"><i class="bx bx-wrench"></i> <span>Skills</span></a></li>
           <li><a href="#resume" class="nav-link scrollto"><i class="bx bx-file-blank"></i> <span>Resume</span></a></li>
           <li><a href="#portfolio" class="nav-link scrollto"><i class="bx bx-book-content"></i> <span>Portfolio</span></a></li>
           <li><a href="#services" class="nav-link scrollto"><i class="bx bx-server"></i> <span>Services</span></a></li>
